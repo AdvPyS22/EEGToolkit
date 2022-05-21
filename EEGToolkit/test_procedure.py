5bfc4a2a357c1492c68fa5bc43b72782dd8ecf16
kdobler <kalvin.dobler@students.unibe.ch>
2022-05-21 15:06:02 +0200

    added some unittests

---
+++ EEGToolkit/test_procedure.py
@@ -0,0 +1,29 @@
+"""
+This script performs some unit testing.
+"""
+
+import unittest
+
+from EEGToolkit.EEGData import EEGData
+
+# General class for performing some unit tests
+class Testclass(unittest.TestCase):
+
+    signal_path = 'data/eeg.npy'
+    event_path = 'data/events.npy'
+    sampling_frequency = 500
+    data = EEGData(signal_path, event_path, sampling_frequency)
+
+    def test_adjust_timsteps(self):
+        assertTupleEqual(data._adjust_timesteps(-0.5, 1), (-250, 500), "Start frame and stop frame should be -250 and 500")
+
+    def test_set_nevents(self):
+        assertListEqual(data.events, [0, 1], "The list should be [0, 1]")
+
+    def test_suffix(self):
+        assertTrue(data._filesuffix(signal_path), 'npy', "The suffix should be 'npy'")
+        assertTrue(data._filesuffix(event_path), 'npy', "The suffix should be 'npy'")
+
+
+if __name__ == "__main__":
+    unittest.main()
